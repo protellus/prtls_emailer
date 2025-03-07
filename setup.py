3f3e83b45867c86048acb1c7248456c9a34340b4
protellus <129015214+protellus@users.noreply.github.com>
2025-03-07 12:04:35 -0500

    Adding dependencies on putils

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="emailer", 
+    name="emailer",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "django>=3.2",  # Ensure Django is installed when using the package
+        "django>=3.2",
+        "requests",
+        "html2text",
+        "putils @ git+https://github.com/protellus/putils.git@main",
     ],
 )
