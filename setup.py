a6c613fb4703744a38c53309bb8155454e2534da
protellus <129015214+protellus@users.noreply.github.com>
2025-03-08 09:51:11 -0500

    Refactoring

--- setup.py
+++ setup.py
@@ -2,28 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="emailer",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "asgiref==3.7.2",
-        "certifi==2023.7.22",
-        "charset-normalizer==3.2.0",
-        "Django==3.2",  # 🔥 Downgrade to match project
-        "djangorestframework==3.14.0",
-        "idna==3.4",
-        "pytz==2025.1",
-        "requests==2.31.0",
-        "sqlparse==0.4.4",
-        "tzdata==2025.1",
-        "urllib3==1.26.16",  # 🔥 Downgrade to match project
-        "oauth @ git+https://github.com/protellus/oauth.git@66849f190a27604acd453b32680fd5412a1d9669",
-        "putils @ git+https://github.com/protellus/putils.git@0db043d85012dec3919488a279b5601d172c1046"
+        "asgiref==3.7.2"
+        "Django==3.2"
+        "packaging==24.2"
+        "pytz==2025.1"
+        "sqlparse==0.4.4"
+        "tzdata==2025.1"
     ],
-    extras_require={
-        "dev": [
-            "black",
-            "pytest",
-        ]
-    },
 )
