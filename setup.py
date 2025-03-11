a8cc74f2778166950c8c57ad6d86b7f3cc5a496f
protellus <129015214+protellus@users.noreply.github.com>
2025-03-11 15:04:15 -0400

    Remove GITHUB token, authentication now SHH Key

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prtls_emailer",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -10,7 +10,7 @@ setup(
         "Django==3.2",
         "djangorestframework==3.14.0",
         "packaging==24.2",
-        "prtls_utils @ git+https://${GITHUB_TOKEN}@github.com/protellus/prtls-utils.git@b07369acd8c3b06d3bd3f62701da6396b899304c",
+        "prtls_utils @ git+https://github.com/protellus/prtls-utils.git@b07369acd8c3b06d3bd3f62701da6396b899304c",
         "pytz==2023.3",
         "sqlparse==0.4.4",
         "tzdata==2025.1",
