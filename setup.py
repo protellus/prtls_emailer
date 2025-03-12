73cadee408bb9455abb5c4520bf5acfc482257d8
protellus <129015214+protellus@users.noreply.github.com>
2025-03-12 16:48:10 -0400

    Versioning for pinned depencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prtls_emailer",
-    version="0.1.11",
+    version="0.1.12",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
