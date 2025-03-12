e388c2703a865c03e51617cbaf9eeb399ca02ca5
protellus <129015214+protellus@users.noreply.github.com>
2025-03-12 19:56:25 -0400

    LICENSE plus security patches

--- setup.py
+++ setup.py
@@ -2,17 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name="prtls_emailer",
-    version="0.1.13",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
         "asgiref==3.7.2",
-        "Django==3.2",
+        "certifi==2025.1.31",
+        "charset-normalizer==3.4.1",
+        "Django>=4.2.20,<5.1.7"
         "djangorestframework==3.14.0",
+        "html2text==2024.2.26",
+        "idna==3.10",
         "packaging==24.2",
         "prtls-utils @ git+https://github.com/protellus/prtls-utils.git@main",
         "pytz==2023.3",
+        "requests==2.32.0",
+        "setuptools-scm==8.2.0",
         "sqlparse==0.4.4",
         "tzdata==2025.1",
+        "urllib3==2.3.0"
     ],
 )
