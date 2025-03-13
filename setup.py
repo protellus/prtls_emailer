92ee31b78fb8ec06d56641d80896cf6cdccdac5c
protellus <129015214+protellus@users.noreply.github.com>
2025-03-12 20:33:36 -0400

    FIxing versions

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         "asgiref==3.7.2",
-<<<<<<< HEAD
         "certifi==2025.1.31",
         "charset-normalizer==3.4.1",
         "Django>=4.2.20,<5.1.7"
@@ -20,14 +19,6 @@ setup(
         "requests==2.32.0",
         "setuptools-scm==8.2.0",
         "sqlparse==0.4.4",
-=======
-        "Django==4.2.20",
-        "djangorestframework==3.15.2",
-        "packaging==24.2",
-        "prtls-utils @ git+https://github.com/protellus/prtls-utils.git@main",
-        "pytz==2023.3",
-        "sqlparse==0.5.0",
->>>>>>> 9240a98498e83acf18fdfb8cd920107a96823d86
         "tzdata==2025.1",
         "urllib3==2.3.0"
     ],
